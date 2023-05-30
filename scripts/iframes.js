755ddc7920561023b051658b194671e0094e82cb
Ishan <98932677+Ishan1522@users.noreply.github.com>
2023-05-30 12:34:28 -0400

    added stuff for titi website

---
+++ scripts/iframes.js
@@ -0,0 +1,17 @@
+function resizeIFrameToFitContent(iFrame) {
+    let videosDiv = document.getElementById("videos-div");
+    iFrame.width = videosDiv.getBoundingClientRect().width;
+    if (window.getComputedStyle(videosDiv).display === "flex") {
+      iFrame.height = videosDiv.getBoundingClientRect().width * (9 / 30);
+    } else {
+      iFrame.height = videosDiv.getBoundingClientRect().width * (18 / 30);
+    }
+  }
+  
+  window.addEventListener('DOMContentLoaded', function (e) {
+  
+    let iframes = document.querySelectorAll("iframe");
+    for (let i = 0; i < iframes.length; i++) {
+      resizeIFrameToFitContent(iframes[i]);
+    }
+  });
